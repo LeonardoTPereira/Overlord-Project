01154052e2466cc42e38fa24af878dd9c0f7c066
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2022-03-16 11:41:49 -0300

    Creates base structure for extendable quest events and a simple controller to receive them. Adds a placeholder controller and a countable and single collectable examples on how to use them. Also adds sample scene to test this.

---
+++ Assets/Scripts/Game/Quests/PlaceholderQuestController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Game.Quests
+{
+    public class PlaceholderQuestController : MonoBehaviour
+    {
+        [SerializeField] private int countableQuestElements = 0;
+        [SerializeField] private int completedTasks = 0;
+
+        public int CountableQuestElements
+        {
+            get => countableQuestElements;
+            set => countableQuestElements = value;
+        }
+
+        public int CompletedTasks
+        {
+            get => completedTasks;
+            set => completedTasks = value;
+        }
+
+        private void OnEnable()
+        {
+            AbstractQuestElement.QuestElementEventHandler += UpdateQuest;
+        }
+
+        private void OnDisable()
+        {
+            AbstractQuestElement.QuestElementEventHandler -= UpdateQuest;
+        }
+
+        private void UpdateQuest(object sender, QuestElementEventArgs args)
+        {
+            if (args is QuestCountableElementEventArgs eventArgs)
+            {
+                CountableQuestElements += eventArgs.Amount;
+            }
+            else
+            {
+                if (args.Success)
+                {
+                    CompletedTasks++;
+                }
+            }
+        }
+    }
+}
