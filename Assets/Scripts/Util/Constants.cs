086ce8af329ccd5d5c87810da87ab73f534f6c16
luizatvieira <luiza.torello@gmail.com>
2022-06-03 22:44:17 -0300

    constants

--- Assets/Scripts/Util/Constants.cs
+++ Assets/Scripts/Util/Constants.cs
@@ -45,28 +45,52 @@ namespace Util
         public const string SECRET_TERMINAL = "secret";
         #endregion
 
+        #region Creativity quests
+        public const string LISTEN = "listen";
+        public const string READ = "read";
+        public const string REPORT = "report";
+        public const string GIVE = "give";
+        public const string EXCHANGE = "exchange";
+        #endregion
+        #region Mastery quests
+        public const string KILL = "kill";
+        public const string CAPTURE = "capture";
+        public const string DAMAGE = "damage";
+        public const string DEFEND = "defend";
+        #endregion
+        #region Achievment quests
+        public const string GATHER = "gather";
+        public const string EXPERIMENT = "experiment";
+        public const string TAKE = "take";
+        public const string USE = "use";
+        public const string REPAIR = "repair";
+        #endregion
+        #region Immersion quests
+        public const string STEALTH = "stealth";
+        public const string ESCORT = "escort";
+        public const string GOTO = "goto";
+        public const string SPY = "spy";
+        #endregion
+
         #region Quest Weights
         public static Func<int,int> OneOptionQuestLineWeight = x => 
         {
             // if ( x < 4 )
             //     return 100;
             // return 0;
-            return (int)(100 - Mathf.Pow(x,2));
+            return (int)(100 - (2*Mathf.Pow(x,2)));
         };
         public static Func<int,int> OneOptionQuestEmptyWeight = x => 
         {
             // if ( x > 4 )
             //     return 100;
             // return 0;
-            return (int) Mathf.Pow(x,2);
+            return (int) (2*Mathf.Pow(x,2));
         };
 
         public static Func<int,int> ThreeOptionQuestLineWeight = x => 
         {
-            if ( x < 4 )
-                return 34;
-            return 0;
-            // return (int)Mathf.Clamp( 0.3f*(1/(x*0.25f)), 0, 30);
+            return OneOptionQuestLineWeight(x)/3;
         };
         public static Func<int,int> ThreeOptionQuestEmptyWeight = x => 
         {
@@ -74,6 +98,16 @@ namespace Util
                 return 100;
             return 0;
             // return (int)Mathf.Clamp( ( 1 -( 1/(x*0.25f))), 0, 100);  
+        };
+
+        public static Func<int,int> FourOptionQuestLineWeight = x =>
+        {
+            return OneOptionQuestLineWeight(x)/4;
+        };
+
+        public static Func<int,int> FiveOptionQuestLineWeight = x =>
+        {
+            return OneOptionQuestLineWeight(x)/5;
         };
         #endregion
 
