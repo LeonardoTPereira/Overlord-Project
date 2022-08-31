2bbf026b52d729c648607970834c76baa2d75442
angelobguido <45051273+angelobguido@users.noreply.github.com>
2022-08-31 09:17:51 -0300

    dungeon: Create extensions for Overlord classes
    Make some inheritance to adapt the top-down game scripts to platform-game scripts.

---
+++ Assets/PlatformGameAssets/Scripts/Dungeon/PlatformDungeonRoom.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using Game.LevelManager;
+using Game.LevelManager.DungeonLoader;
+using UnityEngine;
+using Util;
+
+namespace PlatformGame.Dungeon
+{
+    public class PlatformDungeonRoom : DungeonRoom
+    {
+        public PlatformDungeonRoom(Coordinates coordinates, string code, List<int> keyIDs, int treasure, int totalEnemies, int npc) : base(coordinates, code, keyIDs, treasure, totalEnemies, npc) {} 
+        
+        public new void CreateRoom(Dimensions roomDimensions, RoomGeneratorInput roomGeneratorInput = null)
+        {
+            Dimensions = roomDimensions;
+            InitializeTiles(); // aloca memória para os tiles
+            int roomType = RandomSingleton.GetInstance().Random.Next((int)Enums.RoomPatterns.COUNT);
+            if (roomGeneratorInput == null)
+            {
+                PlatformDefaultRoomCreator.CreateRoomOfType(this, roomType);
+            }
+        }
+        
+        
+        
+    }
+}
---
+++ Assets/PlatformGameAssets/Scripts/Dungeon/PlatformRoomBhv.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game.LevelManager.DungeonManager;
+using UnityEngine;
+
+namespace PlatformGame.Dungeon
+{
+    public class PlatformRoomBhv : RoomBhv
+    {
+        //Change enemy spawners later...
+    }
+}
