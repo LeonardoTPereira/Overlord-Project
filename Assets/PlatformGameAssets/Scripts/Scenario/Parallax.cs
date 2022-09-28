51e775a2f9d3954330aa00f57c7ac92ef6d5736d
Tyago Teoi <tyago.teoi@usp.br>
2022-09-28 02:52:18 -0300

    Refactor platform-game parallax
    Add parallax to the 'y' axis.
    
    Also fix the 'background replication' as the
    player moves. It takes the most far 'block of background'
    and moves it in front of the 'head/tail' of the background,
    where the player is walking to.
    
    The background now seems to be infinite.

--- Assets/PlatformGameAssets/Scripts/Scenario/Parallax.cs
+++ Assets/PlatformGameAssets/Scripts/Scenario/Parallax.cs
@@ -9,28 +9,34 @@ namespace PlatformGame.Scenario
         [SerializeField] private float _backgroundSlotSize;
         [SerializeField] private GameObject _camera;
         [SerializeField] private float _parallaxFactor;
-        private float _startPos;
+
+        private float _xStartPos;
+        private float _yStartPos;
+        private float _temp;
+        private float _xDistance;
+        private float _yDistance;
 
         void Start()
         {
-            _startPos = this.transform.position.x;
-            //_backgroundSlotSize = GetComponent<SpriteRenderer>().bounds.size.x;
-            Debug.Log(_backgroundSlotSize);
-            _backgroundSlotSize = _backgroundSlotSize;
+            _xStartPos = this.transform.position.x;
+            _yStartPos = this.transform.position.y;
         }
 
         void Update()
         {
-            float temp = _camera.transform.position.x * (1 - _parallaxFactor);
-            float distance = _camera.transform.position.x * _parallaxFactor;
+            _temp = _camera.transform.position.x * (1 - _parallaxFactor);
+            _xDistance = _camera.transform.position.x * _parallaxFactor;
+            _yDistance = _camera.transform.position.y * _parallaxFactor;
 
-            transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
+            transform.position = new Vector3(_xStartPos + _xDistance, _yStartPos + _yDistance, transform.position.z);
 
+            
             // Repositioning of the background sprites
-            if (temp > _startPos + _backgroundSlotSize)
-                _startPos += _backgroundSlotSize;
-            else if (temp < _startPos - _backgroundSlotSize)
-                _startPos -= _backgroundSlotSize;
+            if (_temp > _xStartPos + _backgroundSlotSize * 2)
+                _xStartPos += _backgroundSlotSize * 3;
+            else if (_temp < _xStartPos - _backgroundSlotSize * 2)
+                _xStartPos -= _backgroundSlotSize * 3;
+            
         }
     }
 }
