c7d24003956bf47e81b9504da79e5d7be20c1592
Yuji Teoi <tyagoyuji@gmail.com>
2025-12-30 01:27:08 -0300

    Add player scripts and bullet prefab

---
+++ Assets/SpaceShooterAssets/Scripts/Player/PlayerBullet.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed = 12f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/SpaceShooterAssets/Scripts/Player/PlayerShip.cs
@@ -0,0 +1,262 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class PlayerShip : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 6f;
+    public float slowMoveMultiplier = 0.4f;
+
+    [Header("Shooting")]
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireRate = 0.15f;
+
+    [Header("Lives")]
+    public int maxLives = 3;
+    public float invincibilityTime = 2f;
+
+    [Header("Bomb")]
+    public int maxBombs = 3;
+    public float bombInvincibilityTime = 3f;
+
+    [Header("Screen Bounds")]
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
+
+    [SerializeField]
+
+    private int _currentLives;
+    private int _currentBombs;
+
+    private bool _isInvincible;
+    private float _fireTimer;
+
+    private SpriteRenderer _spriteRenderer;
+    private Collider2D _col;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _col = GetComponent<Collider2D>();
+    }
+
+    void Start()
+    {
+        _currentLives = maxLives;
+        _currentBombs = maxBombs;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleShooting();
+        HandleBomb();
+    }
+
+    #region Movement
+
+    private bool _isMovingUp;
+    private bool _isMovingDown;
+    private bool _isMovingLeft;
+    private bool _isMovingRight;
+
+    public void OnMoveUp(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isMovingUp = true;
+        }
+        else if (context.canceled)
+        {
+            _isMovingUp = false;
+        }
+    }
+
+    public void OnMoveDown(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isMovingDown = true;
+        }
+        else if (context.canceled)
+        {
+            _isMovingDown = false;
+        }
+    }
+
+    public void OnMoveLeft(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isMovingLeft = true;
+        }
+        else if (context.canceled)
+        {
+            _isMovingLeft = false;
+        }
+    }
+
+    public void OnMoveRight(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isMovingRight = true;
+        }
+        else if (context.canceled)
+        {
+            _isMovingRight = false;
+        }
+    }
+
+    private bool _isSlowing = false;
+    public void OnSlow(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isSlowing = true;
+        }
+        else if (context.canceled)
+        {
+            _isSlowing = false;
+        }
+    }    
+    void HandleMovement()
+    {
+        Vector2 direction = Vector2.zero;
+
+        if(_isMovingLeft) direction.x -= 1f;
+        if(_isMovingRight) direction.x += 1f;
+        if(_isMovingUp) direction.y += 1f;
+        if(_isMovingDown) direction.y -= 1f;
+
+        direction = Vector2.ClampMagnitude(direction, 1f);
+
+        float speed = moveSpeed;
+        if (_isSlowing)
+            speed *= slowMoveMultiplier;
+
+        Vector2 newPos = (Vector2)transform.position + direction * speed * Time.deltaTime;
+
+        newPos.x = Mathf.Clamp(newPos.x, minBounds.x, maxBounds.x);
+        newPos.y = Mathf.Clamp(newPos.y, minBounds.y, maxBounds.y);
+
+        transform.position = newPos;
+    }    
+
+    #endregion
+
+    #region Shooting
+
+    private bool _isShooting = false;
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isShooting = true;
+        }
+        else if (context.canceled)
+        {
+            _isShooting = false;
+        }
+    }
+
+    void HandleShooting()
+    {
+        _fireTimer -= Time.deltaTime;
+
+        if (_isShooting && _fireTimer <= 0f)
+        {
+            Shoot();
+            _fireTimer = fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+    }
+    #endregion
+
+    #region Bomb
+    void HandleBomb()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && _currentBombs > 0)
+        {
+            UseBomb();
+        }
+    }
+
+    void UseBomb()
+    {
+        _currentBombs--;
+
+        DestroyAllWithTag("Enemy");
+        DestroyAllWithTag("EnemyBullet");
+        DestroyAllWithTag("Obstacle");
+
+        StartCoroutine(InvincibilityCoroutine(bombInvincibilityTime));
+    }
+
+    void DestroyAllWithTag(string tag)
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject obj in objects)
+        {
+            Destroy(obj);
+        }
+    }
+    #endregion
+
+    #region Damage & Invincibility
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_isInvincible)
+            return;
+
+        if (other.CompareTag("EnemyBullet") || other.CompareTag("Enemy") || other.CompareTag("Obstacle"))
+        {
+            TakeDamage();
+        }
+    }
+
+    void TakeDamage()
+    {
+        _currentLives--;
+
+        if (_currentLives <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            StartCoroutine(InvincibilityCoroutine(invincibilityTime));
+        }
+    }
+
+    IEnumerator InvincibilityCoroutine(float duration)
+    {
+        _isInvincible = true;
+        _col.enabled = false;
+
+        float timer = 0f;
+        while (timer < duration)
+        {
+            _spriteRenderer.enabled = !_spriteRenderer.enabled;
+            yield return new WaitForSeconds(0.1f);
+            timer += 0.1f;
+        }
+
+        _spriteRenderer.enabled = true;
+        _col.enabled = true;
+        _isInvincible = false;
+    }
+
+    void Die()
+    {
+        // TODO: explosion, game over, respawn logic
+        Destroy(gameObject);
+    }
+    #endregion
+}
